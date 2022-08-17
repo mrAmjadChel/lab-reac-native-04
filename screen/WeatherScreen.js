41d4c2a16dfb8b4affc5cb2880a8e54788c58178
mrAmjadChel <amjadchelaeh.8@gmail.com>
2022-08-18 05:11:53 +0700

    section 5

---
+++ screen/WeatherScreen.js
@@ -0,0 +1,14 @@
+import react from "react";
+import { StatusBar, View } from "react-native";
+import Weather from "../components/Weather";
+
+
+export default function WeatherScreen({route}) {
+    return (
+        <View>
+            <Weather zipCode={route.params.zipCode} />
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+   
