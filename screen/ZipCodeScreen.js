32319bb647e220ece7736f1611fd0552f5be6ac6
mrAmjadChel <amjadchelaeh.8@gmail.com>
2022-08-18 05:45:19 +0700

    section 6

--- screen/ZipCodeScreen.js
+++ screen/ZipCodeScreen.js
@@ -1,5 +1,6 @@
 import React from "react"
-import { FlatList, Text, View } from "react-native"
+import { FlatList, ImageBackground, StatusBar, StyleSheet, Text, TouchableHighlight, View } from "react-native"
+import { useNavigation } from "@react-navigation/core"
 
 const availableZipItems = [
     { place: 'Hatyai', code: '90110' },
@@ -11,9 +12,9 @@ const availableZipItems = [
 
    const ZipItem = ({place, code, navigation}) => (
         <TouchableHighlight onPress={() => navigation.navigate('Weather', { zipCode: code})}>
-            <View>
-                <Text>{place}</Text>
-                <Text >{code}</Text>
+            <View style={styles.zipItem}>
+                <Text style={styles.zipPlace}>{place}</Text>
+                <Text style={styles.zipCode}>{code}</Text>
             </View>
         </TouchableHighlight>
     )
@@ -22,13 +23,43 @@ const availableZipItems = [
    export default function ZipCodeScreen(){
         const navigation = useNavigation()
         return (
-            <View>
+            <ImageBackground source={require('../bg2.jpg')} style={styles.backdrop}>
                 <FlatList
                     data={availableZipItems}
                     keyExtractor={_keyExtractor}
                     renderItem={({item}) => <ZipItem {...item} navigation={navigation}/>}
                 />
                 <StatusBar style="auto" />
-            </View>
+            </ImageBackground>
         );
    }
+
+   const styles = StyleSheet.create({
+    zipItem: {
+        alignItems:'center',
+        flexDirection:'column',
+        justifyContent:'center',
+    },
+    zipPlace: {
+        fontSize:20,
+        color:'ocean',
+    },
+    zipCode: {
+        fontSize:20,
+        color:'white',
+    },
+    backdrop: {
+        flexDirection:'column',
+        justifyContent:'center',
+        alignItems:'center',
+        width: '100%',
+        height: '100%',
+    },
+    top: {
+        marginTop:30,
+        marginBottom:30,
+        fontSize:50,
+        textAlign:'center',
+        color:'pink',
+    }
+   });
